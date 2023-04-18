f1113f55ad1aed1f9e50004341838e5f1305759b
“Rajat <rajatvermaa95@gmail.com>
2023-04-18 22:37:27 +0530

    Chapter 4 (Common Matchers) - Done

--- multiply.test.js
+++ multiply.test.js
@@ -38,3 +38,22 @@ describe('test multiply positive scenarios',()=>{
     });
 
 })
+
+describe('test multiply negative scenarios',()=>{
+
+    test('multiply 4*3 should not be equal to 11', ()=>{
+        expect(multiply(4,3)).not.toBe(11);
+    });
+
+})
+
+
+test('object example', ()=>{
+    const data = {first: 1};
+    data['second'] = 2;
+
+    expect(data).toEqual({first:1, 'second':2}); 
+    expect(data).toEqual({first:1, second:2}); 
+
+});
+
